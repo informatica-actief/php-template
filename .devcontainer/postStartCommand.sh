058a89b43ef67da88553e1329701f143c055744d
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2026-04-07 23:00:08 +0200

    comment

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-// start webserver without cache on port 8080 and leave it running in background
+// start php server on port 8080 and leave it running in background
 nohup bash -c 'php -S localhost:8000 > .nohup_phpserver.out 2>&1 & rm nohup.out &' 
